6b2c391a5a069d28bfe65cd3c5e7a2a38a73db39
Sam Pakulski <spakulsk@uvm.edu>
2017-04-20 10:58:23 -0400

    Fixed statements

---
+++ test2.cpp
@@ -0,0 +1,110 @@
+#include "Piece.h";
+// Red Team
+Spy bspyOne(false);
+
+Normal_Piece btwoOne(2, false);
+Normal_Piece btwoTwo(2, false);
+Normal_Piece btwoThree(2, false);
+Normal_Piece btwoFour(2, false);
+Normal_Piece btwoFive(2, false);
+Normal_Piece btwoSix(2, false);
+Normal_Piece btwoSeven(2, false);
+Normal_Piece btwoEight(2, false);
+
+Three bthreeOne(false);
+Three bthreeTwo(false);
+Three bthreeThree(false);
+Three bthreeFour(false);
+Three bthreeFive(false);
+
+Normal_Piece bfourOne(4, false);
+Normal_Piece bfourTwo(4, false);
+Normal_Piece bfourThree(4, false);
+Normal_Piece bfourFour(4, false);
+
+Normal_Piece bfiveOne(5, false);
+Normal_Piece bfiveTwo(5, false);
+Normal_Piece bfiveThree(5, false);
+Normal_Piece bfiveFour(5, false);
+
+Normal_Piece bsixOne(6, false);
+Normal_Piece bsixTwo(6, false);
+Normal_Piece bsixThree(6, false);
+Normal_Piece bsixFour(6, false);
+
+Normal_Piece bsevenOne(7, false);
+Normal_Piece bsevenTwo(7, false);
+Normal_Piece bsevenThree(7, false);
+
+
+Normal_Piece beightOne(8, false);
+Normal_Piece beightTwo(8, false);
+
+Normal_Piece bnineOne(9, false);
+
+Normal_Piece btenOne(10, false);
+
+Normal_Piece bBombOne(99, false);
+Normal_Piece bBombTwo(99, false);
+Normal_Piece bBombTwo(99, false);
+Normal_Piece bBombTwo(99, false);
+Normal_Piece bBombTwo(99, false);
+Normal_Piece bBombTwo(99, false);
+
+Normal_Piece bflag(0, false);
+
+// Blue Team
+Spy rspyOne(true);
+
+Normal_Piece rtwoOne(2, true);
+Normal_Piece rtwoTwo(2, true);
+Normal_Piece rtwoThree(2, true);
+Normal_Piece rtwoFour(2, true);
+Normal_Piece rtwoFive(2, true);
+Normal_Piece rtwoSix(2, true);
+Normal_Piece rtwoSeven(2, true);
+Normal_Piece rtwoEight(2, true);
+
+Three rthreeOne(true);
+Three rthreeTwo(true);
+Three rthreeThree(true);
+Three rthreeFour(true);
+Three rthreeFive(true);
+
+Normal_Piece rfourOne(4, true);
+Normal_Piece rfourTwo(4, true);
+Normal_Piece rfourThree(4, true);
+Normal_Piece rfourFour(4, true);
+
+Normal_Piece rfiveOne(5, true);
+Normal_Piece rfiveTwo(5, true);
+Normal_Piece rfiveThree(5, true);
+Normal_Piece rfiveFour(5, true);
+
+Normal_Piece rsixOne(6, true);
+Normal_Piece rsixTwo(6, true);
+Normal_Piece rsixThree(6, true);
+Normal_Piece rsixFour(6, true);
+
+Normal_Piece rsevenOne(7, true);
+Normal_Piece rsevenTwo(7, true);
+Normal_Piece rsevenThree(7, true);
+
+
+Normal_Piece reightOne(8, true);
+Normal_Piece reightTwo(8, true);
+
+Normal_Piece rnineOne(9, true);
+
+Normal_Piece rtenOne(10, true);
+
+Normal_Piece rBombOne(99, true);
+Normal_Piece rBombTwo(99, true);
+Normal_Piece rBombTwo(99, true);
+Normal_Piece rBombTwo(99, true);
+Normal_Piece rBombTwo(99, true);
+Normal_Piece rBombTwo(99, true);
+
+Normal_Piece rflag(0, true);
+
+
